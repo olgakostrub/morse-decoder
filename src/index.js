dd9abc520958d59960c0daca7c0152d70db41fac
Olga Kostrub <lady.irony.ua@gmail.com>
2020-03-22 16:49:13 +0200

    feat: add decode function for Morse decoder

--- src/index.js
+++ src/index.js
@@ -1,46 +1,68 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0"
 };
 
 function decode(expr) {
-    // write your solution here
+  const encodeString = str => {
+    let encodedString = str.replace(/\./g, "10");
+    encodedString = encodedString.replace(/\-/g, "11");
+    const diff = 10 - encodedString.length;
+    if (diff) {
+      encodedString = "0".repeat(diff) + encodedString;
+    }
+    return encodedString;
+  };
+
+  const MORSE_TABLE_DIGITIZED = {};
+  MORSE_TABLE_DIGITIZED["**********"] = " ";
+  for (let i in MORSE_TABLE) {
+    MORSE_TABLE_DIGITIZED[encodeString(i)] = MORSE_TABLE[i];
+  }
+
+  // decode
+  let decodedString = "";
+  for (let i = 0; i < expr.length; i += 10) {
+    const codedString = expr.substr(i, 10);
+    decodedString = decodedString + MORSE_TABLE_DIGITIZED[codedString];
+  }
+  return decodedString;
 }
 
 module.exports = {
-    decode
-}
+  decode
+};
